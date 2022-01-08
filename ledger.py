ad74417ff3721e10778994e416b591a26268380f
chiragshokeen <chirag.shokeen007@gmail.com>
2022-01-08 13:10:08 +0530

    commit

--- ledger.py
+++ ledger.py
@@ -39,4 +39,31 @@ class Blockchain:
         encoded_block = json.dumps(block).encode()
 
         return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self,chain): 
+
+        prev_block = chain[0]
+        block_idx = 1 
+
+        while block_idx<len(chain):
+            block = chain[block_idx]
+
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            
+            prev_proof = prev_block['proof']
+            proof=block['proof']
+
+            hash_val = hashlib.sha256(str( proof**2 - prev_proof**2 ).encode()).hexdigest()
+            if hash_val[:4] != '0000':
+                return False
+
+            prev_block = block
+            block_idx+=1
+
+        return True
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
         
