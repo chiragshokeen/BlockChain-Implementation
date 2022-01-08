24b318c07d4c3cb57ae9f3e560658efed48b05cd
chiragshokeen <chirag.shokeen007@gmail.com>
2022-01-08 11:11:02 +0530

    proof_of_work

--- ledger.py
+++ ledger.py
@@ -20,4 +20,23 @@ class Blockchain:
                  'prev_hash': prev_hash}
         self.chain.append(block)
         return block
+
+    def proof_of_work(self , prev_proof):
+
+        new_proof = 1 
+        check_proof = False
+        while check_proof is False:
+            hash_val = hashlib.sha256( str(new_proof**2 - prev_proof**2).encode()).hexdigest()
+
+            if hash_val[:4] == '0000' :
+                check_proof = True
+            else:
+                new_proof+=1
+        
+        return new_proof
+
+    def hash(self , block):
+        encoded_block = json.dumps(block).encode()
+
+        return hashlib.sha256(encoded_block).hexdigest()
         
